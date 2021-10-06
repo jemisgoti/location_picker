d2032da9ea5fd7f5402f73ebd8025e26c727fa96
Jemis Goti <jemis.g@xcitech.in>
2021-10-06 17:39:54 +0530

    pre development

---
+++ lib/location_picker.dart
@@ -0,0 +1,6 @@
+library location_picker;
+
+export 'src/location_picker.dart';
+export 'src/model/location_result.dart';
+export 'src/utils/location_utils.dart';
+export 'package:geolocator/geolocator.dart';
